b9da06df13f3de66195811c2e87e9864038b198f
jacob5555555555 <jacobeliasprinz@gmail.com>
2018-02-18 16:39:05 -0500

    Made things more general, lets see if it works

--- V3program.py
+++ V3program.py
@@ -20,16 +20,16 @@ def listsAddingTo(length, total):
 				yield [i] + rest
 	
 
-def makeEVals(a, d, numIntervals):
+def makeEVals(d, numIntervals):
 	"""Returns a bunch of LpVariable objects of all variables from the paper.
 	Remember, for f(m,s), a = s and d = m-s"""
 	eVals = {}
 	for index in listsAddingTo(numIntervals, 3):
 		name = "e_" + str(index)
-		eVals[index] = LpVariable(name, 0, 2*d, LpInteger) #number of 3 students is at most 2d when V=3
+		eVals[index] = LpVariable(name, 0, 2*d, LpInteger) #number of 3 students is 2d when V=3
 	return eVals
 
-def makeEasyConstraints(eVals, a, d, intervalTotals):
+def makeEasyConstraints(eVals, d, intervalTotals):
 	sums = [0] * len(intervalTotals)
 	total = 0
 	for index in eVals:
@@ -37,17 +37,67 @@ def makeEasyConstraints(eVals, a, d, intervalTotals):
 		for i in range(len(sums)):
 			sums[i] += index[i] * eVar
 		total += eVar
-	#note that bill wanted total == 6*d because he says s3 == 2d, but this is nonsense
-	#TODO: figure out why bill was wrong
-	V = interval.findV(a+d, a)
-	(sV, sVm1) = interval.getShares(a+d, a, V)
-	return [I1sum == a+d, I2sum == a+d, I3sum == 2*d-a, I4sum == 2*d-a, total == 2*d]
+	intervalConstraints = [sum == intervalTotal for (sum, intervalTotal) in zip(sums, intervalTotals)]
+	return intervalConstraints + [total == 2*d] #there are 2d 3-students
 
-def findCutPoints(eVals, a, d, k=0):
+def findCutPoints(eVals, intervals):
 	"""Finds values of X where below that value, some e variable must be 0.
 	returns (list of constraints that always happen, cut list)
-	cut list is ordered list of (Xcut, constraint)"""
+	cut list is ordered list of (Xcut, constraint)
+	intervals is a list of [(min,max), (min,max)]
+	where min and max are sympy expressions with X as a variable"""
 
+	always = []
+	cuts = []
+
+	#Loop through each e variable, and find the minimum X that allows it to be nonzero.
+	for index in eVals: 
+		eVar = eVals[index]
+
+		#minimum amount of muffin that can be found in given intervals from index
+		minValue = sum(numIntervals * interval[0] for numInInterval, interval in zip(index, intervals))
+		#maximum amount of muffin that can be found in given intervals from index
+		maxValue = sum(numIntervals * interval[1] for numInInterval, interval in zip(index, intervals))
+
+		#X values which will allow such a student to exist
+		allowedXValues = sympy.solve([minValue <= total, total <= maxValue], X).as_set()
+
+		#note 2a/5 below. Bill sent me this bound in an email. Is this actually where X should start?
+		if allowedXValues == sympy.EmptySet() or allowedXValues.end <= 0:# 2*a/5:#if the constraint can never be met or is smaller than X can be
+			always.append(eVar == 0)#add constraint that variable is zero
+		else:#otherwise if the constraint can be met
+			cuts.append((allowedXValues.end, eVar == 0))#add on (min val of X where constraint can't be met, constraint that variable is zero)
+	cuts = sorted(cuts)
+	return (always, cuts)
+			
+def findX(d, intervals, totals):
+	"""intervals is a list of intervals [(min(X), max(X)), (min(X), max(X))],
+	totals is a list of the same length which has all of the total amount of peices in the intervals"""
+	prob = LpProblem("The problem", LpMinimize) #define pulp problem
+
+	eVals = makeEVals(d, len(intervals))
+	constraints = makeEasyConstraints(eVals, d, totals)
+	for constraint in constraints:
+		prob += constraint
+	
+	(always, cuts) = findCutPoints(eVals, intervals)
+
+	for constraint in always: #NOTE FOR DEBUGGING LATER: ITS POSSIBLE THESE LINES WERE COMMENTED OUT LAST MEETING
+		prob += constraint
+	
+	for cut in cuts:
+		print("Trying cut " + str(cut[0]))
+		#print(prob)
+		status = prob.solve()
+		if status != 1:
+			print("No solution found, moving to next cut and adding constraint")
+			prob += cut[1]
+		else:
+			print("found solution, returning")
+			prob += cut[1]
+			#return cut[0]
+
+def makeFirstCaseIntervals(a,d,k):
 	S = sympy.S
 	a, d, k = S(a), S(d), S(k)
 	
@@ -63,49 +113,4 @@ def findCutPoints(eVals, a, d, k=0):
 
 	#The four intervals reffered to in the paper. Note that 0 indexing means e.g. interval 2 is Intervals[1]
 	Intervals = [(values[0], values[1]), (values[1], values[2]), (values[4], values[5]), (values[5], values[6])]
-
-	always = []
-	cuts = []
-
-	#Loop through each e variable, and find the minimum X that allows it to be nonzero.
-	for key in eVals: 
-		(j1, j2, j3, j4) = key
-		eVar = eVals[key]
-
-		minValue = j1 * Intervals[0][0] + j2 * Intervals[1][0] + j3 * Intervals[2][0] + j4 * Intervals[3][0]
-		maxValue = j1 * Intervals[0][1] + j2 * Intervals[1][1] + j3 * Intervals[2][1] + j4 * Intervals[3][1]
-
-		interval = sympy.solve([minValue <= total, total <= maxValue], X).as_set()
-
-		#note 2a/5 below. Bill sent me this bound in an email. Is this actually where X should start?
-		if interval == sympy.EmptySet() or interval.end <= 0:# 2*a/5:#if the constraint can never be met or is smaller than X can be
-			always.append(eVar == 0)#add constraint that variable is zero
-		else:#otherwise if the constraint can be met
-			cuts.append((interval.end, eVar == 0))#add on (min val of X where constraint can't be met, constraint that variable is zero)
-	cuts = sorted(cuts)
-	return (always, cuts)
-			
-def findX(a,d,k=0):
-	prob = LpProblem("The problem", LpMinimize) #define pulp problem
-
-	eVals = makeEVals(a, d, 4) #HARD CODED CHANGE
-	constraints = makeEasyConstraints(eVals, a, d) #HARD CODED CHANGE
-	for constraint in constraints:
-		prob += constraint
-	
-	(always, cuts) = findCutPoints(eVals, a, d, k)
-
-	#for constraint in always:
-	#	prob += constraint
-	
-	for cut in cuts:
-		print("Trying cut " + str(cut[0]))
-		#print(prob)
-		status = prob.solve()
-		if status != 1:
-			print("No solution found, moving to next cut and adding constraint")
-			prob += cut[1]
-		else:
-			print("found solution, returning")
-			prob += cut[1]
-			#return cut[0]
+	return Intervals
