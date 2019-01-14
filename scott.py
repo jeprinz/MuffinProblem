a960fd00a069b492e6c8c077f9d92766ae6f47be
Jacob Prinz <jacobeliasprinz@gmail.com>
2019-01-13 22:53:52 -0500

    Scott's algorithm part 1 complete and working
    Still need to make it construct procedures.

--- scott.py
+++ scott.py
@@ -1,7 +1,14 @@
 #implementation of Scott Huddleton's algorithm for the muffin problem
 
+#TODO: f(19,11) doesn't work. I think its because it should stop after 0 iterations somehow
+#also, neither does f(11,9)
+#f(11,6) gives negative output
+#f(52,31) doesn't work AND ITS ONE OF ERIK's EXAMPLES
+#f(47,14) is the same deal as above
+
 from fractions import Fraction
 import math
+import interval
 
 
 def scott(muffins, majors, minors):
@@ -33,36 +40,51 @@ def scott(muffins, majors, minors):
     #the number L calculated below.
 
     #Assumption: If we can't make chains like that, then we have already found the constraint on the muffin problem.
-    #Below, we check this:
-    if Ns2 * Ps2 >= Nm + Ns2:
+    #Below, we check if its possible to make chains. The following equation does so:
+
+    print("scott((%s),(%s),(%s))"%(pfmap(muffins), pfmap(majors), pfmap(minors)))
+
+    if Ns2 * Ps2 >= Nm + Ns2 or Ns2 == 0:
         return Vs1 / Ps1
 
     # this is the value of L which keeps Nm/Ns2 as close as possible to (Ps2*L - L + 1) / L.
     #That makes sense because it allows the average piece size in the chains to be as large as possible (TODO: check that that statement makes sense)
-    L = math.ceil(1/(Nm/Ns2 + 1 - Ps2))
+    L = Fraction(math.ceil(1/(Nm/Ns2 + 1 - Ps2)))
 
-    #Given the above assumptions and value of L, we can calculate the number of L-length chains there are, a,
-    #and the number of (L-1)-length chains, b. We can get two equations by looking at
+    #Given the above assumptions and value of L, we can calculate the number of L-length chains there are, b,
+    #and the number of (L-1)-length chains, a. We can get two equations by looking at
     # the total number of minors and muffins involved in each chain. The use linear algegra to get below equations.
-    a = l*Nm - l*Ps2*Ns2 + l*Ns2 - Ns2
-    b = (Ns2 - a*(l - 1)) / l
+    a = L*Nm - L*Ps2*Ns2 + L*Ns2 - Ns2
+    b = (Ns2 - a*(L - 1)) / L
 
     #But how do we actually make things work out with major type muffins, minor type muffins, and whatnot? Recursion!
     #We consider a new "scott muffin problem" with the old majors as new muffins, and the (L-1)-chains and L-chain as the
     #majors and minors. The old majors "give pieces to" the old minors.
 
-    #The L-chains will be majors, unless Ps2 = 2 and Vm >= Vs2. Its unclear to me if either of those conditions are possible,
-    #but just to be safe I code them below
+    #The L-chains will be majors unless possibly p = 2 and Vm > Vs2. I may have made a miscalculation, though, because
+    #In practice that seems to be wrong and it seems like the L-chains are just always the majors.
 
     #The following are the correct values of N, V, P for the L-chains and (L-1)-chains
-    newMajors = (a, (L*Ps2 - (L-1))*Vm - L*Vs2, L*Ps2 - 2*(L-1))
-    newMinors = (b, (L*Ps2 - (L-2))*Vm - (L-1)*Vs2, (L-1)*Ps2 - 2*(L-2))
+    newPs1 = (L*Ps2 - 2*(L-1))*(Pm-1) + (L - 1) * (Pm - 2)
+    newPs2 = ((L-1)*Ps2 - 2*(L-2))*(Pm-1) + (L - 2) * (Pm - 2)
+    newMajors = (b, (L*Ps2 - (L-1))*Vm - L*Vs2, newPs1)
+    newMinors = (a, ((L-1)*Ps2 - (L-2))*Vm - (L-1)*Vs2, newPs2)
 
-    if Ps2 = 2 and Vm >= Vs2:
-        newMajors, newMinors = newMinors, newMajors
+    print("L = %s, a = %s, b = %s" %(L,a,b))
 
     return scott(majors, newMajors, newMinors)
 
 
 def f(m,s):
-    
+    m = Fraction(m)
+    s = Fraction(s)
+    V = interval.findV(m,s)
+    sV, sVm1 = interval.getShares(m,s,V)
+    #sV are majors, sVm1 are minors
+    return scott((m,1,2),(sV,m/s,V),(sVm1,m/s,V-1))
+
+def pfmap(fractions):
+    return str(tuple(map(pf, fractions)))
+
+def pf(fraction):
+    return "%s/%s"%(str(fraction.numerator), str(fraction.denominator))
